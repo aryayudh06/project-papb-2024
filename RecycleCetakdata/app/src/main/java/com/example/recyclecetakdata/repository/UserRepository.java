f1daff918f7f6998cacdd94322908237b1aaa307
bintangnurs <127941540+bintangnurs@users.noreply.github.com>
2024-05-13 17:33:46 +0700

    Update UserRepository.java

--- RecycleCetakdata/app/src/main/java/com/example/recyclecetakdata/repository/UserRepository.java
+++ RecycleCetakdata/app/src/main/java/com/example/recyclecetakdata/repository/UserRepository.java
@@ -11,23 +11,21 @@ import com.example.recyclecetakdata.model.User;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-// fungsi repository untuk akses database
-// logika bisnis seperti mengambil data dari database tidak diletakkan di UI
 public class UserRepository {
-    private final Dao mUsersDao; // membuat variabel dao
-    private final ExecutorService executorService; // membuat variabel executorService untuk handle operasi background yang berhubungan dengan database
-    public UserRepository(Application application) { // constructor
-        MyDatabase db = MyDatabase.getDatabase(application); // membuat instance database
-        mUsersDao = db.userDao(); // set variabel dao dengan memanggil dao dari database
-        executorService = Executors.newSingleThreadExecutor(); // set executorService
+    private final Dao mUsersDao; 
+    private final ExecutorService executorService; 
+    public UserRepository(Application application) { 
+        MyDatabase db = MyDatabase.getDatabase(application); 
+        mUsersDao = db.userDao(); 
+        executorService = Executors.newSingleThreadExecutor(); 
     }
 
     public LiveData<List<User>> getAllUsers() {
         return mUsersDao.getAllData();
-    } // fungsi untuk mengambil semua data dalam bentuk list
+    } 
 
     public void insert(List<User> user) {
         executorService.execute(() -> mUsersDao.insert(user));
-    } // fungsi untuk insert semua data Array ke room database
+    } 
 
 }
