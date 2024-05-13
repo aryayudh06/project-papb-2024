b60afa3dcdf9ec15a7d853600199790e7cb17f03
bintangnurs <127941540+bintangnurs@users.noreply.github.com>
2024-05-13 17:32:06 +0700

    Update MainViewModel.java

--- RecycleCetakdata/app/src/main/java/com/example/recyclecetakdata/viewmodel/MainViewModel.java
+++ RecycleCetakdata/app/src/main/java/com/example/recyclecetakdata/viewmodel/MainViewModel.java
@@ -11,24 +11,21 @@ import com.example.recyclecetakdata.repository.UserRepository;
 
 import java.util.List;
 
-// ViewModel adalah komponen penting dalam arsitektur Model-View-ViewModel (MVVM)
-// yang membantu memisahkan logika UI dari logika bisnis.
-// viewModel di bawah berfungsi untuk handle data di UI
-// Urutannya Database > Repository > ViewModeal > UI (Activity)
+
 
 public class MainViewModel extends AndroidViewModel {
-    private UserRepository mUserRepository; // membuat variable repository
+    private UserRepository mUserRepository; 
 
-    public MainViewModel(Application application) { // constructor
+    public MainViewModel(Application application) { 
         super(application);
-        mUserRepository = new UserRepository(application); // set nilai variable repository
+        mUserRepository = new UserRepository(application); 
     }
 
     public LiveData<List<User>> getAllUsers() {
         return mUserRepository.getAllUsers();
-    } // fungsi untuk mendapatkan semua data yang nantinya dapat di akses di Activity
+    } 
 
     public void insert(List<User> user) {
         mUserRepository.insert(user);
-    } // fungsi untuk input semua data yang nantinya dapat di akses di Activity
+    } 
 }
