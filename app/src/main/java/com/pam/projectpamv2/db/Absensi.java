e3859cfcc5377fce4a115bf5e4f68c32f2a99e3d
Steven Hot Asi Sihite <stesi2702@gmail.com>
2024-05-27 21:14:03 +0700

    Implementasi Firebase

---
+++ app/src/main/java/com/pam/projectpamv2/db/Absensi.java
@@ -0,0 +1,65 @@
+package com.pam.projectpamv2.db;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+public class Absensi {
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    @ColumnInfo(name = "nama")
+    public String nama = "";
+
+    @ColumnInfo(name = "alamat")
+    public String alamat = "";
+
+    @ColumnInfo(name = "jamAbsensi")
+    public String jamAbsensi = "";
+
+    @ColumnInfo(name = "tanggal")
+    public String tanggal = "";
+
+    // Required empty constructor
+    @Ignore
+    public Absensi() { }
+
+    public Absensi(String nama, String alamat, String jamAbsensi, String tanggal) {
+        this.nama = nama;
+        this.alamat = alamat;
+        this.jamAbsensi = jamAbsensi;
+        this.tanggal = tanggal;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return this.nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat(){
+        return this.alamat;
+    }
+
+    public void setAlamat(){
+        this.alamat = alamat;
+    }
+
+    public String getJamAbsensi(){
+        return this.jamAbsensi;
+    }
+
+    public void setJamAbsensi(){
+        
+    }
+}
--- app/src/main/java/com/pam/projectpamv2/ui/DataAbsensi.java
+++ app/src/main/java/com/pam/projectpamv2/ui/DataAbsensi.java
@@ -3,14 +3,26 @@ package com.pam.projectpamv2.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.pam.projectpamv2.R;
 
 public class DataAbsensi extends AppCompatActivity {
+
+    private Button btBack;
+
+    FirebaseDatabase db;
+    DatabaseReference dbRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_absensi);
+
+        db = FirebaseDatabase.getInstance();
+        dbRef = db.getReference("absensi");
+
     }
 }
